8b7468af70eaba996c3c56fcc5b2fc55e342236a
gowtham-r-web <814724243048@trp.srmtrichy.edu.in>
2026-01-30 11:25:40 +0530

    Create FREQUENCY DISTRIBUTIONS, AVERAGES, VARIABILITY exp3.py

---
+++ FREQUENCY DISTRIBUTIONS, AVERAGES, VARIABILITY exp3.py
@@ -0,0 +1,26 @@
+# Python program to get average of a list
+# Importing the NumPy module import
+numpy as np
+# Taking a list of elements
+list = [2, 40, 2, 502, 177, 7, 9]
+# Calculating average using average()
+print(np.average(list))
+Output:
+105.57142857142857
+# Python program to get variance of a list
+# Importing the NumPy module import
+numpy as np
+# Taking a list of elements list = [2, 4,
+4, 4, 5, 5, 7, 9]
+# Calculating variance using var()
+print(np.var(list))
+Output:
+4.0
+# Python program to get standard deviation of a list
+# Importing the NumPy module import
+numpy as np
+# Taking a list of elements list = [290,
+124, 127, 899]
+# Calculating standard #
+deviation using var()
+print(np.std(list))
